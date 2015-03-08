925734bf1da7d82adaa1a7b24fb599e493ef9397
[content deleted] <chin.alan.q@gmail.com>
2015-03-08 01:18:28 -0800

    Initial commit
    Strictly .cs files

---
+++ Assets/GameGenerator.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGenerator : MonoBehaviour {
+
+	public GameObject jenga_blocks;
+	public GameObject block;
+	public Vector3 start;
+	public int num_sets;
+
+	// Use this for initialization
+	void Start () {
+		Physics.gravity = new Vector3(0.0f, -9.81f, 0.0f);
+
+		for (uint i = 0; i < num_sets; i++) {
+			// Jenga size
+			Vector3 j_size = block.transform.localScale;
+
+			// Left, center, right jenga positions
+			Vector3 l_pos = start + new Vector3(-j_size.x, j_size.y / 2 + i * j_size.y, 0);
+			Vector3 c_pos = start + new Vector3(0, j_size.y / 2 + i * j_size.y, 0);
+			Vector3 r_pos = start + new Vector3(j_size.x, j_size.y / 2 + i * j_size.y, 0);
+
+			// Jenga objects
+			GameObject l_go = (GameObject) Instantiate(block, l_pos, Quaternion.identity);
+			GameObject c_go = (GameObject) Instantiate(block, c_pos, Quaternion.identity);
+			GameObject r_go = (GameObject) Instantiate(block, r_pos, Quaternion.identity);
+
+			// Names
+			l_go.name = "Left Jenga";
+			c_go.name = "Middle Jenga";
+			r_go.name = "Right Jenga";
+
+			// Add Jenga objects to set
+			GameObject set = new GameObject();
+			set.name = "Jenga Set " + (i + 1);
+			l_go.transform.parent = set.transform;
+			c_go.transform.parent = set.transform;
+			r_go.transform.parent = set.transform;
+
+			// Rotate every other set
+			float rad = 0.01f;
+			float rand = -rad + (Random.value * (2 * rad));
+			if((i % 2) == 0) set.transform.Rotate(new Vector3(0.0f, 90.0f + rand, 0.0f));
+
+			// Add to block container
+			set.transform.parent = jenga_blocks.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Hydra.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hydra : MonoBehaviour {
+
+	public GameObject cursor;
+	public GameObject camera;
+	public GameObject dock;
+	public float height = 200.0f;
+	public float left_center = 45.0f;
+	public float left_height = 200.0f;
+
+	bool wasDocked = false;
+	bool toReset = false;
+	Vector3 rest_offset;
+	Vector3 rest_pos;
+
+	// Use this for initialization
+	void Start () {
+		/*
+		if (SixenseInput.IsBaseConnected(0)) {
+			print ("Right controller connected");
+		} else {
+			print ("Please connect Razer Hydra");
+			Application.Quit();
+		}
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Hydra
+		for(uint i = 0; i < 2; i++) {
+			if(SixenseInput.Controllers[i] != null && SixenseInput.Controllers[i].Enabled) {
+				switch (i) {
+				case 0: // Left controller
+					if(SixenseInput.Controllers[i].GetButton(SixenseButtons.BUMPER)) {
+						if(SixenseInput.Controllers[i].Position.x < left_center) {
+							camera.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+						}
+						else {
+							camera.transform.RotateAround(Vector3.zero, Vector3.up, -20 * Time.deltaTime);
+						}
+					} 
+					if(SixenseInput.Controllers[i].GetButton(SixenseButtons.TRIGGER)) {
+						if(SixenseInput.Controllers[i].Position.y < left_height) {
+							if(camera.transform.position.y > 7)
+								camera.transform.Translate(new Vector3(0, -0.5f, 0));
+						}
+						else {
+							camera.transform.Translate(new Vector3(0, 0.5f, 0));
+						}
+					}
+					break;
+				case 1: // Right controller
+					rest_pos = dock.transform.localPosition;
+					rest_pos.y += 0.75f;
+					//cursor.transform.rotation = SixenseInput.Controllers[i].Rotation;
+					//cursor.transform.Rotate(SixenseInput.Controllers[i].Rotation.eulerAngles);
+					
+					if(!wasDocked) {
+						if(SixenseInput.Controllers[i].Docked) {
+							rest_offset = SixenseInput.Controllers[i].Position;
+							//rest_pos = cursor.transform.position;
+							wasDocked = true;
+						}
+					}
+					else if(wasDocked && SixenseInput.Controllers[i].Docked) {
+						if(toReset) Application.LoadLevel(0);
+						cursor.transform.localPosition = rest_pos;
+					}
+					else {
+						if(!SixenseInput.Controllers[i].Docked && wasDocked) toReset = true;
+						Vector3 v = (SixenseInput.Controllers[i].Position - rest_offset);
+						v.Scale(new Vector3(0.1f, 0.1f, 0.5f ));
+			
+						cursor.transform.localPosition = rest_pos + v;
+					}
+					break;
+				}
+			}
+		}
+	}
+}
